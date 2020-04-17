1bc7dffee7268211e5482952e78343eaff05476b
Raees Iqbal <raees.bhatti@gmail.com>
2020-04-17 10:26:19 -0700

    Initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+public class Sudo
+{
+    public static void Main()
+    {
+        Console.WriteLine("something");
+
+        ProcessStartInfo processInfo = new ProcessStartInfo();
+        processInfo.Verb = "runas";
+        processInfo.FileName = "notepad.exe";
+        try
+        {
+            Process.Start(processInfo);
+        }
+        catch (Win32Exception)
+        {
+            //Do nothing. Probably the user canceled the UAC window
+        }
+    }
+}
